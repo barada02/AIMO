1c2fa5d69e39a14257222a4ab793e1cbe79b71cf
Kumar <chandanbarada2@gmail.com>
2026-04-05 17:10:34 +0530

    code executor agent working

--- DataForZV2/dataforge/agent.py
+++ DataForZV2/dataforge/agent.py
@@ -1,8 +1,15 @@
 from google.adk.agents.llm_agent import Agent
+from google.adk.code_executors import BuiltInCodeExecutor
 
 root_agent = Agent(
     model='gemini-2.5-flash',
     name='root_agent',
-    description='A helpful assistant for user questions.',
-    instruction='Answer user questions to the best of your knowledge',
+
+    code_executor=BuiltInCodeExecutor(),
+
+    instruction="""You are a calculator agent.
+    When given a mathematical expression, write and execute Python code to calculate the result.
+    Return the final numerical result and code in json.
+    """,
+    description="Executes Python code to perform calculations.",
 )
